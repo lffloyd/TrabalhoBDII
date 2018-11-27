7bc0e55674a1060129b16e711f23f9c9dff461ed
redrookie <justgamessites@hotmail.com>
2018-11-26 22:01:38 -0200

    Update parte2.sql

--- parte2.sql
+++ parte2.sql
@@ -55,3 +55,26 @@ INSERT INTO Track (TrackId, Name, AlbumId, MediaTypeId, GenreId, Composer, Milli
 (3513, 'zsszsz', 1, 1, 1, 'Angus Young, Malcolm Young, Brian Johnson', 343719, 11170334, 0.99);
 
 delete from Track where trackid > 3503;
+
+/*A regra semântica abaixo é semelhante a anterior. Ela é uma stored procedure que um dado usuário criado chamado newUser 
+deve utilizar para poder adicionar TRACKS as suas PLAYLISTS. Ele não possui acesso à nenhuma informação das tabelas, apenas pode
+adicionar TRACKS novas utilizando a stored procedure que recebe como parâmetros o ID da PLAYLIST e o ID da TRACK. Caso o nº de TRACKS
+numa playlist tenha atingido o limite de 50, a stored procedure levanta um erro 
+avisando ao usuário sobre o limite de 50 TRCKS por PLAYLIST.*/
+
+CREATE USER newUser
+IDENTIFIED BY 1234;
+GRANT CONNECT TO newUser;
+GRANT EXECUTE ON insert_track_on_playlist TO newUser;
+
+create or replace procedure insert_track_on_playlist(playlist_id int, track_id int)
+is
+total int;
+BEGIN
+select count(*) into total from PLAYLIST where PLAYLISTID = playlist_id;
+IF (total>=50) THEN
+    raise_application_error(-23, 'Playlists can contain a maximum of 50 tracks.');
+ELSE
+insert into PLAYLISTTRACK(PLAYLISTID,TRACKID) values (playlist_id, track_id);
+END IF;
+END;
